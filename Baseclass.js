c795595b46500f05e8544a6245181abeee8d75f3
SamGeorge80 <70869113+SamGeorge80@users.noreply.github.com>
2020-12-31 15:25:46 -0500

    Add files via upload

---
+++ Baseclass.js
@@ -0,0 +1,21 @@
+class BaseClass{
+    constructor(x, y, width, height, angle) {
+        var options = {
+            'restitution':0.8,
+            'friction':1.0,
+            'density':1.0
+        }
+        this.body = Bodies.rectangle(x, y, width, height, options);
+        this.width = width;
+        this.height = height;
+        World.add(world, this.body);
+      }
+      display(){
+        var angle = this.body.angle;
+        push();
+        rectMode(CENTER);
+        fill("brown");
+        rect(pos.x, pos.y, this.width, this.height);
+        pop();
+      }
+}
